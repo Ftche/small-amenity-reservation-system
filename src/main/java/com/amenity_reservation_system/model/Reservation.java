80e6ea8d8638c3b61f77343b4c9be6caf61b3250
unknown <franklintche@yahoo.fr>
2021-11-02 18:28:19 +0100

    commit the project

---
+++ src/main/java/com/amenity_reservation_system/model/Reservation.java
@@ -0,0 +1,71 @@
+package com.amenity_reservation_system.model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import javax.persistence.*;
+
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reservation {
+
+    @Id
+    @Column(nullable = false, updatable = false)
+    @SequenceGenerator(
+            name = "primary_sequence",
+            sequenceName = "primary_sequence",
+            allocationSize = 1,
+            initialValue = 10000
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "primary_sequence"
+    )
+    private Long id;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Column(nullable = false)
+    private LocalDate reservationDate;
+
+    @DateTimeFormat(pattern = "HH:mm")
+    @Column
+    private LocalTime startTime;
+
+    @DateTimeFormat(pattern = "HH:mm")
+    @Column
+    private LocalTime endTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AmenityType amenityType;
+
+    @Column(nullable = false, updatable = false)
+    private OffsetDateTime dateCreated;
+
+    @Column(nullable = false)
+    private OffsetDateTime lastUpdated;
+
+    @PrePersist
+    public void prePersist() {
+        dateCreated = OffsetDateTime.now();
+        lastUpdated = dateCreated;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        lastUpdated = OffsetDateTime.now();
+    }
+
+}
